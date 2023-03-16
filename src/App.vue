172f342379e4b28cf7a6cd50f32bee1ea31ff695
Ryan <656040874@qq.com>
2023-03-16 13:42:23 +0800

    基本完成初探

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>666</h1>
+  <h2>
+    <span>count is {{ count }} *2={{ double }}</span>
+    <button @click="count++">++</button>
+  </h2>
+</template>
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const count = ref(6);
+    function add() {
+      count.value++;
+    }
+    const double = computed(() => count.value * 2);
+    return { count, add, double };
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { createApp, h } from "vue";
+// const App = {
+//   render() {
+//     return h("div", null, [h("div", null, String("123"))]);
+//   },
+// };
+// createApp(App).mount("#app");
+
+import { createApp } from "vue"; // node_module
+import App from "./App.vue";
+import "./index.css";
+createApp(App).mount("#app");
